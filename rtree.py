8c84fea8ce6d58362ce2eed9c7026c18d692eb7d
wolperite <kakaoninja@gmx.at>
2013-07-30 10:52:48 +0200

    Update rtree.py
    works now

--- rtree.py
+++ rtree.py
@@ -1,8 +1,26 @@
 import turtle as t
-import random as r
+#import random as r
 
-def rtree (age=100):
+def rbaum (age=100):
     """paint a recursiv tree"""
-    pass
+    if age < 5:
+        return
+    t.pd()
+    t. fd(age)
+    t. rt(45)
+    rbaum(age/2)
+    t. lt(90)
+    rbaum(age/2)
+    t. rt(45)
+    t. pu()
+    t. fd(-age)
+    return
 
-rtree ()
+if __name__=="__main__":
+    t. speed(0)
+    t. home()
+    t. clear()
+    t. lt(90)
+    t. pu()
+    t. fd(-200)
+    rbaum ()
